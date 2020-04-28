c687bec15a9bfb337da5b073a4fe2d4f81803f1e
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-04-29 00:16:57 +0300

    templates configured

--- E_shop/shopApp/views.py
+++ E_shop/shopApp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    return render(request,"shop/index.html")
+
+
+def product(request):
+    return render(request,"shop/product.html")
