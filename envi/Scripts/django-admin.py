5ba872c24f6b337fa4d10600b3c8a1df0c631d27
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-04-28 23:46:00 +0300

    repo initialized

---
+++ envi/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Bakhtiyar Garashov\Desktop\E_commerce\envi\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
