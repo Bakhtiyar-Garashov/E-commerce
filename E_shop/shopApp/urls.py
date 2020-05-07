f20d8f5b4d0218e4bc80062b9c49ab5591b10866
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-05-07 15:32:39 +0300

    title blocks added

--- E_shop/shopApp/urls.py
+++ E_shop/shopApp/urls.py
@@ -1,10 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from .views import index,product,about
+from .views import home,product,about
 
 urlpatterns = [
-   path('',index,name='home'),
+   path('',home,name='home'),
    path('products',product,name='product'),
    path('about',about,name='about'),
 ]
--- E_shop/shopApp/views.py
+++ E_shop/shopApp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-def index(request):
+def home(request): # this is home page view
     return render(request,"shop/home.html")
 
 
-def product(request):
+def product(request): #this is products page view
     return render(request,"shop/product.html")
 
-def about(request):
+def about(request): # thos is about page view
     return render(request,"shop/about.html")
