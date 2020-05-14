5140aa562a0594946f4be5e888955226c8cefadc
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-05-14 15:14:06 +0300

    products rendered in homeview

--- E_shop/shopApp/views.py
+++ E_shop/shopApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,HttpResponse
 
+from .models import Product
+
 # Create your views here.
 def home(request): # this is home page view
-    return render(request,"shop/home.html")
+    products=Product.objects.all()
+    return render(request,"shop/home.html",context={'products':products})
 
 
 def product(request): #this is products page view
