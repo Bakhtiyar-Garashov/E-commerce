90969a73826b2841a50f018af3be03b02e60da2a
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-05-15 20:21:18 +0300

    Search based on category added

--- E_shop/shopApp/urls.py
+++ E_shop/shopApp/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from .views import home,product,about,contact
+from .views import home,product,about,contact,getCategoryData
 
 urlpatterns = [
    path('',home,name='home'),
    path('products',product,name='product'),
    path('about',about,name='about'),
    path('contact',contact,name='contact'),
+   path('categories/<str:name>',getCategoryData,name='getCategoryData'),
 ]
--- E_shop/shopApp/views.py
+++ E_shop/shopApp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render,HttpResponse
+from django.http import request
 
-from .models import Product
+from .models import Product,Category
 
 # Create your views here.
 def home(request): # this is home page view
     products=Product.objects.all()
-    return render(request,"shop/home.html",context={'products':products})
+    categories=Category.objects.all()
+    return render(request,"shop/home.html",context={'products':products,'categories':categories})
+
+def getCategoryData(request,name): # this is home page view
+    categories=Category.objects.all()
+    category_searched=Category.objects.get(name=name)
+    certain_category_entries=Product.objects.filter(category=category_searched.id)
+   
+    return render(request,"shop/home.html",context={'products':certain_category_entries,'categories':categories})
+
 
 
 def product(request): #this is products page view
