b938c47726ec655128331bc1df578db88d7c6d22
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-06-06 22:13:45 +0300

    card and checkout template added

--- E_shop/shopApp/urls.py
+++ E_shop/shopApp/urls.py
@@ -1,18 +1,19 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home,product,about,contact,getCategoryData
+from .views import home, product, about, contact, getCategoryData, card
 
 urlpatterns = [
-   path('',home,name='home'),
-   path('products',product,name='product'),
-   path('about',about,name='about'),
-   path('contact',contact,name='contact'),
-   path('categories/<str:name>',getCategoryData,name='getCategoryData'),
+    path('', home, name='home'),
+    path('products', product, name='product'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('card', card, name='card'),
+    path('categories/<str:name>', getCategoryData, name='getCategoryData'),
 ]
 
 
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- E_shop/shopApp/views.py
+++ E_shop/shopApp/views.py
@@ -1,28 +1,37 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.http import request
 
-from .models import Product,Category
+from .models import Product, Category
 
 # Create your views here.
-def home(request): # this is home page view
-    products=Product.objects.all()
-    categories=Category.objects.all()
-    return render(request,"shop/home.html",context={'products':products,'categories':categories})
-
-def getCategoryData(request,name): # this is home page view
-    categories=Category.objects.all() 
-    category_searched=Category.objects.get(name=name) # first to find category object by name
-    certain_category_entries=Product.objects.filter(category=category_searched.id) # find related products based on category id
-    return render(request,"shop/home.html",context={'products':certain_category_entries,'categories':categories})
 
 
-
-def product(request): #this is products page view
-    return render(request,"shop/product.html")
-
-def about(request): # this is about page view
-    return render(request,"shop/about.html")
+def home(request):  # this is home page view
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request, "shop/home.html", context={'products': products, 'categories': categories})
 
 
-def contact(request): # this is contact page view
-    return render(request,"shop/contact.html")
+def getCategoryData(request, name):  # this is home page view
+    categories = Category.objects.all()
+    # first to find category object by name
+    category_searched = Category.objects.get(name=name)
+    certain_category_entries = Product.objects.filter(
+        category=category_searched.id)  # find related products based on category id
+    return render(request, "shop/home.html", context={'products': certain_category_entries, 'categories': categories})
+
+
+def product(request):  # this is products page view
+    return render(request, "shop/product.html")
+
+
+def about(request):  # this is about page view
+    return render(request, "shop/about.html")
+
+
+def contact(request):  # this is contact page view
+    return render(request, "shop/contact.html")
+
+
+def card(request):  # this is card page view
+    return render(request, "shop/card.html")
